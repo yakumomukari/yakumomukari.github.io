18b8c3f8fbd9304f637111e4a3c3ce9683c017b4
yakumomukari <1923579853@qq.com>
2025-10-26 19:01:07 +0800

    Site updated: 2025-10-26 19:01:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761474104106";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1761476465570";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
